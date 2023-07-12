1b239dab1ddd49d7057115a218de08a6d8eaf59d
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2023-07-12 23:48:25 +0200

    added namespace to airflow

--- deployment/modules/airflow/main.tf
+++ deployment/modules/airflow/main.tf
@@ -11,12 +11,12 @@ locals {
 data "aws_caller_identity" "current" {}
 data "aws_region" "current" {} # 
 
-# resource "kubernetes_namespace" "airflow" {
-#   metadata {
+resource "kubernetes_namespace" "airflow" {
+  metadata {
 
-#     name = var.namespace
-#   }
-# }
+    name = var.namespace
+  }
+}
 
 ####### LOG STORAGE
 

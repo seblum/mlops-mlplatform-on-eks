b2aa88f9daa381b2ea34d4c4fdca12f8ac6f85ec
Sebastian Blum <sebastian.blum@alexanderthamm.com>
2023-08-09 11:57:27 +0200

    clean grafana deployment

--- deployment/modules/monitoring/main.tf
+++ deployment/modules/monitoring/main.tf
@@ -1,23 +1,9 @@
-
-
-
-# resource "helm_release" "prometheus" {
-#   name      = var.tag_name # "prometheus"
-#   namespace = var.tag_name
-
-#   repository = var.helm_chart_repository # "https://prometheus-community.github.io/helm-charts"
-#   chart      = var.helm_chart_name # "kube-prometheus-stack"
-#   version    = var.helm_chart_version #"40.0.0"
-# }
-
-
 resource "helm_release" "prometheus" {
   chart            = "prometheus"
   name             = "prometheus"
   namespace        = var.namespace
   create_namespace = var.create_namespace
 
-  # https://github.com/prometheus-community/helm-charts/
   repository = "https://prometheus-community.github.io/helm-charts"
   version    = "19.7.2"
 
@@ -38,19 +24,75 @@ resource "helm_release" "prometheus" {
   ]
 }
 
-
 resource "helm_release" "grafana" {
   chart            = "grafana"
   name             = "grafana"
   namespace        = var.namespace
   create_namespace = var.create_namespace
 
-  # https://github.com/prometheus-community/helm-charts/
   repository = "https://grafana.github.io/helm-charts/"
   version    = "6.57.4"
 
   values = [
-    "${file("${path.module}/helm/grafana/values.yaml")}"
-  ]
-
+    yamlencode({
+      datasources = {
+        yaml = {
+          apiVersion = 1
+          datasources = [
+            {
+              name      = "Prometheus"
+              type      = "prometheus"
+              url       = "http://prometheus-server.monitoring.svc.cluster.local"
+              access    = "proxy"
+              isDefault = true
+            }
+          ]
+        }
+      }
+      service = {
+        enabled = true
+        type    = "LoadBalancer"
+      }
+      # admin:
+      #   ## Name of the secret. Can be templated.
+      #   existingSecret: ""
+      #   userKey: admin-user
+      #   passwordKey: admin-password
+      dashboardProviders = {
+        yaml = {
+          apiVersion = 1
+          providers = [
+            {
+              name   = "'default'"
+              orgId  = 1
+              folder = "''"
+              type : "file"
+              disableDeletion : false
+              editable : true
+              options = {
+                path = "/var/lib/grafana/dashboards/default"
+              }
+            }
+          ]
+        }
+      }
+      dashboards = {
+        default = {
+          prometheus-stats = {
+            gnetId     = 2
+            revision   = 2
+            datasource = "Prometheus"
+          }
+          prometheus-stats-2 = {
+            gnetId     = 315
+            datasource = "Prometheus"
+          }
+          k8s-cluster = {
+            gnetId     = 6417
+            datasource = "Prometheus"
+          }
+        }
+      }
+  })]
+  # "${file("${path.module}/helm/grafana/values.yaml")}"
 }
